ed489ad3db7f3ea5f75fc6c465cb5cae7c5e1285
yanhua.chen <yanhua.chen@bkjk.com>
2018-07-12 11:05:40 +0800

    同步方法不能被继承

---
+++ chapter02/src/main/java/com/cyh/sync/method/SyncMethodCannotBeExtended.java
@@ -0,0 +1,65 @@
+package com.cyh.sync.method;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author CYH
+ * @date 2018/07/12
+ *
+ * 同步方法不能被继续
+ */
+public class SyncMethodCannotBeExtended {
+
+
+    public static void main(String[] args) {
+        Sub sub = new Sub();
+        MyThread myThread = new MyThread(sub);
+        myThread.setName("A");
+        myThread.start();
+
+        MyThread myThreadB = new MyThread(sub);
+        myThreadB.setName("B");
+        myThreadB.start();
+    }
+
+    static class MyThread extends Thread {
+        private Sub sub;
+
+        public MyThread(Sub sub) {
+            this.sub = sub;
+        }
+
+        @Override
+        public void run() {
+            this.sub.serviceMethod();
+        }
+    }
+
+    static class Main {
+        synchronized void serviceMethod() {
+            System.out.println("Main " + Thread.currentThread().getName() + " -begin- " + new Date());
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Main " + Thread.currentThread().getName() + " -end- " + new Date());
+        }
+    }
+
+    static class Sub extends Main {
+        @Override
+        void serviceMethod() {
+            System.out.println("Sub " + Thread.currentThread().getName() + " -begin- " + new Date());
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Sub " + Thread.currentThread().getName() + " -end- " + new Date());
+            super.serviceMethod();
+        }
+    }
+
+}
